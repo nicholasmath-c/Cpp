b2e84416a12adba91582aa3deaacfd18557c1838
Nicholas M. A. Costa <59261158+Mus4ng@users.noreply.github.com>
2020-11-21 05:37:58 -0300

    Add files via upload

---
+++ Exercício 3 - Sequência.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <locale.h>
+using namespace std;
+
+int main(){
+	setlocale(LC_ALL, "Portuguese"); //Correção da codificação de caracteres para o português
+	
+	int n; //Quantidade de termos
+	int a; //Auxiliador na contagem de algarismos 
+	int num1 = 1;
+	int num2 = 3;
+	
+	cout << "Informe a quantidade de N termos que deseja para iniciar a sequência: ";
+	cin >> n;
+	
+	int sequel[n]; //Vetor com a sequência numérica
+
+
+	while(true){
+		sequel[a] = num1;
+		a++;
+		num1++;
+		if(a == n){break;}
+		
+		sequel[a] = num2;
+		a++;
+		if(a == n){break;}
+		
+		sequel[a] = num2;
+		a++;
+		num2 = num1 + 3;
+		if(a == n){break;}
+	}
+	
+	//Impressão da sequência
+	for(int i = 0; i < n; i++){
+		if(i == 0){
+			cout << sequel[i];
+		} 
+		else{
+			cout <<  ", " << sequel[i];
+		}
+	}
+}
